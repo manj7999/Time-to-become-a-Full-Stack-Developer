08da681506712ccbae341be8cd175fd5cfdba498
Ankit Manjhi <ankitmanjhi286@gmail.com>
2024-03-24 21:31:57 +0530

    Prerendering the powers of Next JS

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import MeetupDetails from "../../components/meetups/MeetupDetails";
 
 function MeetupDetails() {
   return (
@@ -10,5 +10,42 @@ function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticProps() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1',
+                },
+            },
+            {
+                params: {
+                    meetupId: 'm2',
+                },
+            },
+        ],
+    };
+}
+
+export async function getStaticProps(context) {
+    // fetch data for a single meetup
+
+    const meetupId = context.params.meerupId;
+    console.log(meetupId);
+
+    return {
+      props: {
+        meetupData: {
+            image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+            id: meetupId,
+            title: 'First Meetup',
+            address: 'Some Strret 5, Some City',
+            description: 'This is a first meetup'
+        },
+      },
+    };
+};
 
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -21,8 +20,32 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return  <MeetupList meetups={DUMMY_MEETUPS} />
+function HomePage(props) {
+  return  <MeetupList meetups={props.meetups} />
+
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: { 
+//     meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 1
+  };
 }
 
 export default HomePage;
